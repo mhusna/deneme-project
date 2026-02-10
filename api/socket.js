3e776392b56296eda96785cb4e5cb204fec3c5f3
mhusna <mehmethusna@outlook.com>
2026-02-10 21:19:50 +0300

    test commiti

--- api/socket.js
+++ api/socket.js
@@ -1,37 +1,3 @@
-import { Server } from "socket.io";
-
 export default function handler(req, res) {
-  if (!res.socket.server.io) {
-    const io = new Server(res.socket.server, {
-      path: "/api/socket",
-      cors: {
-        origin: [
-          "https://deneme-project.vercel.app",
-          "https://deneme-project-git-main-husnas-projects.vercel.app"
-        ],
-        methods: ["GET", "POST"]
-      }
-    });
-
-    res.socket.server.io = io;
-
-    io.on("connection", (socket) => {
-      console.log("Kullanıcı bağlandı:", socket.id);
-
-      // Sürekli veri gönder (test için)
-      const interval = setInterval(() => {
-        socket.emit("price", {
-          time: new Date().toLocaleTimeString(),
-          value: Math.random() * 100
-        });
-      }, 2000);
-
-      socket.on("disconnect", () => {
-        clearInterval(interval);
-        console.log("Kullanıcı ayrıldı:", socket.id);
-      });
-    });
-  }
-
-  res.end();
+  res.status(200).json({ ok: true, message: "Socket endpoint çalışıyor" });
 }
